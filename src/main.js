65e7f11ca47ea6e0a485545d2301ebd96d08cc27
lyg <342017921@qq.com>
2018-12-21 18:32:37 +0800

    as usual

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import HashTable from './data-structure/hash-table/HashTable'
+
+
+let hs = new HashTable()
+window.hs = hs
+hs.set("a",0)
+hs.set("ab",1)
+hs.set("ccccc",1)
+hs.set("zadswwwxxzz",5)
+hs.set("cccbd",2)
